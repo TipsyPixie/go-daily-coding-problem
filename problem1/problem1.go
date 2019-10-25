84e5ab711e26a4117776f9b9388f1d5fc1ca2a63
Seongjae Hwang <lotsofluck4m@gmail.com>
2019-10-26 02:14:27 +0900

    reformatted

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -1,14 +1,12 @@
 package problem1
 
-func Run(numbers []uint, k uint) bool {
-    candidates := make(map[uint]bool)
+func Run(numbers []int, k int) bool {
+    candidates := make(map[int]bool)
     for _, n := range numbers {
-        if n < k {
-            if candidates[n] {
-                return true
-            }
-            candidates[k-n] = true
+        if candidates[n] {
+            return true
         }
+        candidates[k-n] = true
     }
     return false
 }
--- problem1/problem1_test.go
+++ problem1/problem1_test.go
@@ -2,9 +2,21 @@ package problem1
 
 import "testing"
 
-func TestRun(t *testing.T) {
-    result := Run([]uint{10, 15, 3, 7}, 17)
+func TestRunSimple(t *testing.T) {
+    result := Run([]int{10, 15, 3, 7}, 17)
     if !result {
+        t.FailNow()
+    }
+}
+
+func TestRunLong(t *testing.T) {
+    size := 1000000
+    args := make([]int, size, size)
+    for i := 0; i < size; i++ {
+        args[i] = i
+    }
+    result := Run(args, size*2)
+    if result {
         t.FailNow()
     }
 }
