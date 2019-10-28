f4f0d23f9726d925ca7afc849bbeff2e0567249e
Seongjae Hwang <lotsofluck4m@gmail.com>
2019-10-28 22:11:27 +0900

    comments for problem4

--- problem4/problem4.go
+++ problem4/problem4.go
@@ -5,6 +5,7 @@ import (
 )
 
 func Run(input []int) int {
+    // opting out negative values and 0s
     var l, r = 0, len(input)
     for l < r {
         if input[l] > 0 {
@@ -19,6 +20,7 @@ func Run(input []int) int {
     }
     input = input[:l]
 
+    // using indices of input as flags. e.g.) input[0] > 0 => 1 exists, input[1] > 0 => 2 exists
     for _, value := range input {
         if absoluteValue := int(math.Abs(float64(value))); absoluteValue <= len(input) && input[absoluteValue-1] > 0 {
             input[absoluteValue-1] *= -1
