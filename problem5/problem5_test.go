c22caa2ee2d1e0592c970b3d6b4bb20add2aaeb6
Seongjae Hwang <lotsofluck4m@gmail.com>
2019-10-30 02:19:21 +0900

    problem6

--- problem5/problem5_test.go
+++ problem5/problem5_test.go
@@ -2,11 +2,15 @@ package problem5
 
 import "testing"
 
+var pair = Cons(3, 4)
+
 func TestCar(t *testing.T) {
-    pair := Cons(3, 4)
     if Car(pair) != 3 {
         t.Fail()
     }
+}
+
+func TestCdr(t *testing.T) {
     if Cdr(pair) != 4 {
         t.Fail()
     }
