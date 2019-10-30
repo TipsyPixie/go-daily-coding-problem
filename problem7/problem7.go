029910b9f5360b10b1fd2cc23b364c5661064d50
S.Hwang <14135938+TipsyPixie@users.noreply.github.com>
2019-10-31 01:49:16 +0900

    problem7

---
+++ problem7/problem7.go
@@ -0,0 +1,33 @@
+package problem7
+
+import (
+    "strconv"
+)
+
+func Run(input string) int {
+    length := len(input)
+    switch {
+    case length == 1:
+        if asInteger, _ := strconv.Atoi(input); asInteger > 0 && asInteger < 10 {
+            return 1
+        } else {
+            return 0
+        }
+    case length == 2:
+        if asInteger, _ := strconv.Atoi(input); asInteger < 10 || asInteger > 26 {
+            return 0
+        } else if input[1] == '0' {
+            return 1
+        } else {
+            return 2
+        }
+    case length >= 3:
+        answer := Run(input[:length-1]) * Run(input[length-1:])
+        if asInteger, _ := strconv.Atoi(input[length-2:]); asInteger >= 10 || asInteger >= 26 {
+            answer += Run(input[:length-2])
+        }
+        return answer
+    default:
+        return 0
+    }
+}
