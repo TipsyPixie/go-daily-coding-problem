ee339a0e9aed9905d1ede0c5ae78995635a2b5c6
S.Hwang <14135938+TipsyPixie@users.noreply.github.com>
2019-11-02 18:05:16 +0900

    problem9

---
+++ problem9/problem9_test.go
@@ -0,0 +1,27 @@
+package problem9
+
+import "testing"
+
+func TestRun(t *testing.T) {
+    args := []int{2, 4, 6, 2, 5}
+    expected := 13
+    if Run(args) != expected {
+        t.FailNow()
+    }
+}
+
+func TestRun2(t *testing.T) {
+    args := []int{5, 1, 1, 5}
+    expected := 10
+    if Run(args) != expected {
+        t.FailNow()
+    }
+}
+
+func TestRun3(t *testing.T) {
+    args := []int{10, -1, -1, 0, -7, -1, 0, 3, 7}
+    expected := 17
+    if Run(args) != expected {
+        t.FailNow()
+    }
+}
