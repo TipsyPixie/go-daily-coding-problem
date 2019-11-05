c8a2f52d88d0a24fa199dd8e17b0f1bf2d835890
TipsyPixie <lotsofluck4m@gmail.com>
2019-11-05 21:21:17 +0900

    problem11

---
+++ problem11/problem11_test.go
@@ -0,0 +1,18 @@
+package problem11
+
+import (
+	"testing"
+)
+
+func TestTrie(t *testing.T) {
+	trie := NewTrie()
+	trie.Insert("dog").Insert("dear").Insert("deal").Insert("demon").Insert("dope")
+	resultMap := map[string]bool{}
+	for _, result := range trie.Find("de") {
+		resultMap[result] = true
+	}
+	if !resultMap["dear"] || !resultMap["deal"] || !resultMap["demon"] {
+		t.Log(resultMap)
+		t.FailNow()
+	}
+}
