b7a527296cf45c458b79020b6156d856283ee085
S.Hwang <14135938+TipsyPixie@users.noreply.github.com>
2019-11-08 00:46:13 +0900

    reformatted

--- problem014/problem014_test.go
+++ problem014/problem014_test.go
@@ -7,14 +7,14 @@ import (
 
 func TestRun(t *testing.T) {
 	result := Run(10000)
-	if result < math.Pi * 0.95 || result > math.Pi * 1.05 {
+	if result < math.Pi*0.95 || result > math.Pi*1.05 {
 		t.FailNow()
 	}
 }
 
 func TestRun2(t *testing.T) {
 	result := Run(10000000)
-	if result < math.Pi * 0.999 || result > math.Pi * 1.001 {
+	if result < math.Pi*0.999 || result > math.Pi*1.001 {
 		t.FailNow()
 	}
 }
