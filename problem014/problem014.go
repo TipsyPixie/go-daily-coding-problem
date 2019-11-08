2c9b72f3233ab22770a11a6ad2697034573873ea
TipsyPixie <lotsofluck4m@gmail.com>
2019-11-08 23:44:56 +0900

    problem14 concurrency optimized

--- problem014/problem014.go
+++ problem014/problem014.go
@@ -7,24 +7,21 @@ import (
 )
 
 type tester func(float64, float64) bool
-type generator func() float64
 
-func Run(totalRepeat int) float64 {
-	const concurrency = 8
+func Run(concurrency int, totalRepeat int) float64 {
 	rand.Seed(time.Now().UnixNano())
-	circleTester := func(x float64, y float64) bool { return math.Pow(x, 2)+math.Pow(y, 2) < 1 }
-	xGenerator := func() float64 { return rand.Float64() }
 	subResults := make(chan struct{ successCount, trialCount int }, concurrency)
+	isInsideCircle := func(x float64, y float64) bool { return math.Pow(x, 2)+math.Pow(y, 2) < 1 }
+	repeat := totalRepeat / concurrency
+	xSamples, ySamples := sampleCoordinates(repeat * concurrency)
 	for i := 0; i < concurrency; i++ {
-		margin := i
-		yGenerator := func() float64 { return (float64(margin) + rand.Float64()) / float64(concurrency) }
-		go func(testerFunction tester, xGenerator generator, yGenerator generator, repeat int) {
-			successCount, trialCount := simulate(circleTester, xGenerator, yGenerator, repeat)
+		startsAt, endsAt := i*repeat, (i+1)*repeat
+		go func() {
+			successCount, trialCount := simulate(isInsideCircle, xSamples[startsAt:endsAt], ySamples[startsAt:endsAt], repeat)
 			subResults <- struct{ successCount, trialCount int }{successCount: successCount, trialCount: trialCount}
-		}(circleTester, xGenerator, yGenerator, totalRepeat/concurrency)
+		}()
 	}
-	finished := 0
-	totalSuccessCount, totalTrialCount := 0, 0
+	totalSuccessCount, totalTrialCount, finished := 0, 0, 0
 	for finished < concurrency {
 		select {
 		case result, _ := <-subResults:
@@ -37,13 +34,23 @@ func Run(totalRepeat int) float64 {
 	return math.Round(1000*approxPi) / 1000
 }
 
-func simulate(testerFunction tester, xGenerator generator, yGenerator generator, repeat int) (int, int) {
+func simulate(criteria tester, xValues []float64, yValues []float64, loop int) (int, int) {
 	successCount, trialCount := 0, 0
-	for i := 0; i < repeat; i++ {
-		if testerFunction(xGenerator(), yGenerator()) {
+	for i := 0; i < loop; i++ {
+		trialCount++
+		if criteria(xValues[i], yValues[i]) {
 			successCount++
 		}
-		trialCount++
 	}
 	return successCount, trialCount
+}
+
+func sampleCoordinates(loop int) ([]float64, []float64) {
+	xValues := make([]float64, loop, loop)
+	yValues := make([]float64, loop, loop)
+	for i := 0; i < loop; i++ {
+		xValues[i] = rand.Float64()
+		yValues[i] = rand.Float64()
+	}
+	return xValues, yValues
 }
