be9721bdb2323b8d09a8afa19f649b7ade9040b6
S.Hwang <14135938+TipsyPixie@users.noreply.github.com>
2019-11-08 23:44:56 +0900

    problem14 concurrency optimized

--- problem014/problem014_test.go
+++ problem014/problem014_test.go
@@ -6,14 +6,14 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	result := Run(10000)
+	result := Run(1, 10000)
 	if result < math.Pi*0.95 || result > math.Pi*1.05 {
 		t.FailNow()
 	}
 }
 
 func TestRun2(t *testing.T) {
-	result := Run(10000000)
+	result := Run(8, 100000000)
 	if result < math.Pi*0.999 || result > math.Pi*1.001 {
 		t.FailNow()
 	}
